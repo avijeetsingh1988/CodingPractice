e10ef0d59c03b19d1cde71d470f6fe6f79284b02
Avijeet Singh <avijeet_singh17@hotmail.com>
2020-10-06 18:10:03 -0400

    commit to master

--- Python/Importing_modules/package1/module1.py
+++ Python/Importing_modules/package1/module1.py
@@ -1,7 +1,1 @@
-import sys
-print(sys.path)
-
-import package1.module1_2
-
-print('This is module1 under package1')
-print(greeting)
+print('hello')
--- Python/Importing_modules/package1/module1_2.py
+++ Python/Importing_modules/package1/module1_2.py
@@ -1,11 +1,1 @@
-from package2.module2 import greeting 
-
-print('This is module1_2 under package1')
-#greeting='Hello from module1_2'
-def greetings():
-    print('hello')
-
-b=print(greeting)
-
-#C:\Users\avije\Documents\Code Training\Python\Importing_modules\package1\module1_2.py
-#C:\Users\avije\Documents\Code Training\Python\Importing_modules\package2\module2.py
+print('hello')
